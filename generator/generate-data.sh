96627b8389c62e1ec5f4056765df40adcc2d3b9e
Samuel Vandamme <samuel@sava.be>
2021-02-12 13:25:20 +0100

    fix: fixed paths in generator

--- generator/generate-data.sh
+++ generator/generate-data.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-ROOT=../..
-WEBSITE_DIR=${ROOT}/website
+ROOT=..
+WEBSITE_DIR=..
 
 # Clean the data dir
 echo ">"
@@ -15,12 +15,12 @@ echo ">"
 echo "> Copying dashboard data"
 echo ">"
 mkdir ${WEBSITE_DIR}/public/data || true
-cp -v -R ${ROOT}/quickstarts/* ${WEBSITE_DIR}/public/data
+cp -v -R ${ROOT}/library/* ${WEBSITE_DIR}/public/data
 
 # Generate summary file
 echo ">"
 echo "> Generating data for use in website"
 echo ">"
-node generate.js "${ROOT}/quickstarts/"
+node generate.js "${ROOT}/library/"
 echo "> Copying data file to website"
 cp -v data.json ${WEBSITE_DIR}/public/data.json
